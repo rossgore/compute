09d6694e56b581f8550ff59095860a53fc499069
James Muguira <muguira.james@gmail.com>
2020-03-13 12:29:18 -0400

    change the URL

--- python3/stim_hard.py
+++ python3/stim_hard.py
@@ -5,6 +5,6 @@ js = {
     "Y" : [ 1,2,3,4,5,6,7,8,9,10,11,12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 }
 
-r = requests.post('http://localhost:5000/regression', json=js)
+r = requests.post('http://localhost:5000/stats/regression', json=js)
 print(r.json())
 
